8050ad53572b7f26c92449ffaad4a1fe106f5fda
ukemeedidiong <ukemeedidiong226@gmail.com>
2024-11-26 19:33:47 +0100

    first commit

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,20 @@
-// Your code here
+Array.prototype.isEqual = function (otherArray) {
+    // Check if the argument is an array
+    if (!Array.isArray(otherArray)) {
+      return false;
+    }
+
+    // Check if lengths are the same
+    if (this.length !== otherArray.length) {
+      return false;
+    }
+
+    // Compare elements one by one
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== otherArray[i]) {
+        return false;
+      }
+    }
+
+    return true; // All elements match
+  };
